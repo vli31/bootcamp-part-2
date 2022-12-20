7647b9a70b7f58ab48c7aa3b92a5e1f285b8741d
Victoria <victoriali2012@gmail.com>
2022-12-19 19:36:42 -0500

    transferring video parts 1 through 3

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import CardEditor from './CardEditor';
+import CardViewer from './CardViewer';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        { front: 'front1', back: 'back1' },
+        { front: 'front2', back: 'back2' },
+      ],
+      editor: true,
+    };
+  }
+  
+  addCard = card => {
+    const cards = this.state.cards.slice().concat(card);
+    this.setState({ cards });
+  };
+
+  deleteCard = index => {
+    const cards = this.state.cards.slice();
+    cards.splice(index, 1);
+    this.setState({ cards });
+  };
+
+  switchMode = () => this.setState({ editor: !this.state.editor });
+  
+  render() {
+    if (this.state.editor) {
+      return (
+        <CardEditor
+          addCard={this.addCard}
+          cards={this.state.cards}
+          deleteCard={this.deleteCard}
+          switchMode={this.switchMode}
+        />
+      );
+    } else {
+      return <CardViewer switchMode={this.switchMode} />;
+    }
+  }
+}
+
+export default App;
