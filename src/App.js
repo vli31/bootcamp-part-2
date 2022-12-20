395a464bac196865ba11e013162c262a05ebb011
Victoria <victoriali2012@gmail.com>
2022-12-19 19:40:22 -0500

    set up react app properly

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
-import React from 'react';
-import CardEditor from './CardEditor';
-import CardViewer from './CardViewer';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: [
-        { front: 'front1', back: 'back1' },
-        { front: 'front2', back: 'back2' },
-      ],
-      editor: true,
-    };
-  }
-  
-  addCard = card => {
-    const cards = this.state.cards.slice().concat(card);
-    this.setState({ cards });
-  };
-
-  deleteCard = index => {
-    const cards = this.state.cards.slice();
-    cards.splice(index, 1);
-    this.setState({ cards });
-  };
-
-  switchMode = () => this.setState({ editor: !this.state.editor });
-  
-  render() {
-    if (this.state.editor) {
-      return (
-        <CardEditor
-          addCard={this.addCard}
-          cards={this.state.cards}
-          deleteCard={this.deleteCard}
-          switchMode={this.switchMode}
-        />
-      );
-    } else {
-      return <CardViewer switchMode={this.switchMode} />;
-    }
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
